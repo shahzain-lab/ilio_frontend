edf3f84f0bb66afd3d0dee522c974ebedd34934e
shahzain-lab <shahzain30196@gmail.com>
2023-01-04 00:28:27 +0500

    added head tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,12 @@ import Navbar from '../layout/Navbar'
 // redux
 import { store } from '../state/store/store';
 import { Provider } from 'react-redux'
+import AppHead from '../layout/Head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <Provider store={store}>
+  <AppHead title='Home Page' />
   <GlobalStyle />
   <Navbar />
   <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from 'next'
 import ContentsCards from '../components/home/ContentsCards'
-import AppHead from '../layout/Head'
 import { StyledContainer } from '../styles/pages/Home.styles'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <AppHead title='Home Page' />
       <StyledContainer>
         <ContentsCards></ContentsCards>
       </StyledContainer>
-    </>
   )
 }
 
